537f1d6d94c7544d2b588a582a80d5c40637fbd4
hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
2021-06-11 11:41:16 +0700

    Config vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,36 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    book: {
+      bookName: "Lo cc",
+      author: "",
+      releaseDate: null,
+      authorImageFile: null,
+      bookCoverImageFile: null,
+      title: "",
+      content: ""
+    }
+  },
+  mutations: {
+    setBook(state, book) {
+      state.book.bookName = book.bookName;
+      state.book.author = book.actions;
+      state.book.releaseDate = book.releaseDate;
+      state.book.authorImageFile = book.authorImageFile;
+      state.book.bookCoverImageFile = book.bookCoverImageFile;
+      state.book.title = book.title;
+      state.book.content = book.content;
+    }
+  },
+  actions: {
+    async setBook({ commit }, data) {
+      commit("setBook", data);
+    }
+  },
+  getters: {
+    getBook(state) {
+      return state.book;
+    }
+  }
 });
