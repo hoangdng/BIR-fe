398d5d0c16a003143e49f566051753e2010560c5
hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
2021-06-23 17:08:30 +0700

    CAll api to get all the books

--- src/components/TheMainContent.vue
+++ src/components/TheMainContent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-content px-5">
     <div class="columns is-multiline is-3">
-      <BookCard v-for="book in books" :key="book.id" :book="book" />
+      <BookCard v-for="(book, index) in books" :key="index" :book="book" />
     </div>
   </div>
 </template>
@@ -9,6 +9,7 @@
 <script>
 // @ is an alias to /src
 import BookCard from "@/components/BookCard.vue";
+import { getAllBookApi } from "@/http/book.api";
 
 export default {
   name: "TheMainContent",
@@ -20,55 +21,14 @@ export default {
       books: []
     };
   },
-  mounted() {
-    this.renderBookCards();
-  },
-  methods: {
-    renderBookCards() {
-      let fakeData = [
-        {
-          id: 1,
-          title: "Where rainbow ends.",
-          coverImage: null,
-          author: "Cecelina Ahern",
-          description:
-            "Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. "
-        },
-        {
-          id: 2,
-          title: "Where rainbow ends.",
-          coverImage: null,
-          author: "Cecelina Ahern",
-          description:
-            "Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. "
-        },
-        {
-          id: 3,
-          title: "Where rainbow ends.",
-          coverImage: null,
-          author: "Cecelina Ahern",
-          description:
-            "Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. "
-        },
-        {
-          id: 4,
-          title: "Where rainbow ends.",
-          coverImage: null,
-          author: "Cecelina Ahern",
-          description:
-            "Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. "
-        },
-        {
-          id: 5,
-          title: "Where rainbow ends.",
-          coverImage: null,
-          author: "Cecelina Ahern",
-          description:
-            "Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. Eu consectetur velit Lorem id ipsum cillum sunt eiusmod nisi reprehenderit nostrud ipsum. Occaecat consequat magna irure cillum nostrud dolor quis laboris non ea. Ea adipisicing non ad cupidatat culpa ea proident officia dolor dolore minim aliquip id. Reprehenderit deserunt magna tempor aliqua adipisicing. Esse ipsum dolore cillum laboris adipisicing et reprehenderit eu elit dolore excepteur incididunt. "
-        }
-      ];
-      this.books = fakeData;
-    }
+  created: async function() {
+    await getAllBookApi()
+      .then(response => {
+        this.books = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
 </script>
